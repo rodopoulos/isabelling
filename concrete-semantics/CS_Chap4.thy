02f05b9a1be8955c5c578759ce50c66ff8fc1474
Felipe Rodopoulos <felipekss@gmail.com>
2018-01-24 18:32:43 -0200

    Exercise 4.3 redone

--- concrete-semantics/CS_Chap4.thy
+++ concrete-semantics/CS_Chap4.thy
@@ -71,31 +71,43 @@ inductive star' :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<
   refl' : "star' r x x" |
   step' : "star' r x y \<Longrightarrow> r y z \<Longrightarrow> star' r x z"
 
-lemma stars_refl' [simp] : "star r x x \<Longrightarrow> star' r x x"
+(* These two next lemmas are not required, but let's play a little with the definitions*)
+lemma "star r x x \<Longrightarrow> star' r x x"
   apply (induction)
   apply (rule refl')
   apply (simp_all)
   done
 
-lemma stars_refl [simp] : "star' r x x \<Longrightarrow> star r x x"
+lemma "star' r x x \<Longrightarrow> star r x x"
   apply (induction)
   apply (simp_all)
   done
 
-lemma star'_trans [simp]: "star' r y z \<Longrightarrow> r x y \<Longrightarrow> star' r x z"
-  apply (induction rule: star'.induct)
-  apply (auto intro: refl' step')
-  done
-
-theorem star_is_star' : "star r x y \<Longrightarrow> star' r x y"
+(*
+  Ok, now let's prove the first formula. 
+  The lemma below is required for the subgoal left
+  after we apply the reflection rule of star
+*)
+lemma star_trans: "star r x y \<Longrightarrow> r y z \<Longrightarrow> star r x z"
   apply (induction rule: star.induct)
-  apply (auto intro: refl')
+  apply (auto intro: star.refl star.step)
   done
 
-theorem star'_is_star : "star' r x y \<Longrightarrow> star r x y"
+theorem  "star' r x y \<Longrightarrow> star r x y"
   apply (induction rule: star'.induct)
-   apply (auto)
-  apply (meson star_step1 star_trans)
+  apply (rule star.refl)
+  apply (auto simp add: star_trans)
+  done
+
+(* As above, the theorem left a subgoal and we need this lemma *)
+lemma star'_trans: "star' r y z \<Longrightarrow> r x y \<Longrightarrow> star' r x z"
+  apply (induction rule: star'.induct)
+  apply (auto intro: star'.refl' star'.step')
+  done
+
+theorem "star r x y \<Longrightarrow> star' r x y"
+  apply (induction rule: star.induct)
+  apply (auto simp add: star'_trans intro: star'.refl')
   done
 
 end
