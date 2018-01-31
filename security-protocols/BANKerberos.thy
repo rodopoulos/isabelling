608af5bc9fb7329550b1eb12782654106f7e1b6a
Felipe Rodopoulos <felipekss@gmail.com>
2018-01-31 14:02:19 -0200

    Defining entity lifetimes and expiration predicates

--- security-protocols/BANKerberos.thy
+++ security-protocols/BANKerberos.thy
@@ -11,11 +11,18 @@ begin
 
 (* DEFINITION OF CURRENT TIME *)
 abbreviation CT :: "event list \<Rightarrow> nat" where
-  "CT == lenght"
+  "CT == length"
 
 (* DEFINITION OF LIFETIME FOR ENTITIES *)
-consts sesKlife :: nat
-consts authlife :: nat
+consts sesKlife :: nat (* session key *)
+consts authlife :: nat (* authenticators *)
+
+(* DEFINITION OF EXPIRATION PREDICATES *)
+abbreviation expiredK :: "[nat, event list] \<Rightarrow> bool" where
+  "expiredK Tk evs == ((CT evs) - Tk > sesKlife)"
+
+abbreviation expiredA :: "[nat, event list] \<Rightarrow> bool" where
+  "expiredA Ta evs == ((CT evs) - Ta > authlife)"
 
 
 (* PROTOCOL MODEL *)
@@ -32,19 +39,32 @@ inductive_set bankerberos :: "event list set" where
     - Event is valid
     - Session key K is fresh
     - Session key K is symmetric
-    - Agent A must have issued the Server
+    - Step 1 occured: Agent A must have issued the Server
   *)
   BK2: "\<lbrakk> evs2 \<in> bankerberos; Key K \<notin> used evs2; K \<in> symKeys;
         Says A' Server \<lbrace>Agent A, Agent B\<rbrace> \<in> set evs2 \<rbrakk> 
     \<Longrightarrow> Says Server A (Crypt (shrK A) 
           \<lbrace> Number CT evs2, Agent B, Key K, (Crypt (shrK B) 
-            \<lbrace> Number CT evs2, Agent A, Key K\<rbrace>) 
+            \<lbrace> Number CT evs2, Agent A, Key K \<rbrace>) 
           \<rbrace>) # evs2 \<in> bankerberos" |
 
-  (* 3rd step: A send the ticker and the authenticator to B. Hence:
+  (* 3rd step: A send the ticket and the authenticator to B. Hence:
     - Event is valid
-    - 
+    - Step 2 occured
+    - Step 1 occured 
+
+  BK3: "\<lbrakk>
+          evs3 \<in> bankerberos;
+          Says Server A (Crypt (shrK A) \<lbrace> Number Tk, Agent B, Key K, 
+            (Crypt (shrK B) \<lbrace> Number Tk, Agent A, Key K \<rbrace>)
+          \<rbrace>) \<in> set evs3;
+          Says A Server \<lbrace>Agent A, Agent B\<rbrace> \<in> set evs2
+        \<rbrakk>
+    \<Longrightarrow> Says A B \<lbrace>
+          (Crypt (shrK B) \<lbrace> Number CT evs2, Agent A, Key K \<rbrace>, 
+          (Crypt K \<lbrace> Agent A, Number (CT evs3) \<rbrace>)
+        \<rbrace> # evs3 \<in> bankerberos"
   *)
-  "BK3: \<lbrakk> evs3 \<in> bankerberos; \<rbrakk>"
+  
 
 end
