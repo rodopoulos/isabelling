2fd8dff35159985c7fbb7552cc1b969aa45ee75f
Felipe Rodopoulos <felipekss@gmail.com>
2018-03-22 23:26:51 -0300

    NS Public: initting secrecy lemmas

--- security-protocols/Public_NS.thy
+++ security-protocols/Public_NS.thy
@@ -15,19 +15,26 @@ inductive_set nspublic :: "event list set" where
 
   Nil: "[] \<in> nspublic"
 
-  | Fake: "\<lbrakk>evfk \<in> nspublic; X \<in> synth (analz (spies evfk))\<rbrakk>
-    \<Longrightarrow> Says Spy B X # evfk \<in> nspublic"
+  | Fake: "\<lbrakk>evsf \<in> nspublic; X \<in> synth (analz (knows Spy evsf))\<rbrakk>
+    \<Longrightarrow> Says Spy B X # evsf \<in> nspublic"
 
   | NS1: "\<lbrakk>evs1 \<in> nspublic; Nonce Na \<notin> used evs1\<rbrakk> 
-    \<Longrightarrow> Says A B (Crypt (shrK A) \<lbrace>Nonce Na, Agent A\<rbrace>) # evs1 \<in> nspublic"
+    \<Longrightarrow> Says A B (Crypt (pubK B) \<lbrace>Nonce Na, Agent A\<rbrace>) # evs1 \<in> nspublic"
 
   | NS2: "\<lbrakk>evs2 \<in> nspublic; Nonce Nb \<notin> used evs2;
-         Says A B (Crypt (shrK A) \<lbrace>Nonce Na, Agent A\<rbrace>) \<in> set evs2 \<rbrakk> 
-    \<Longrightarrow> Says B A (Crypt (shrK B) \<lbrace>Number Na, Number Nb, Agent B\<rbrace>) # evs2 \<in> nspublic"
+         Says A' B (Crypt (pubK B) \<lbrace>Nonce Na, Agent A\<rbrace>) \<in> set evs2 \<rbrakk> 
+    \<Longrightarrow> Says B A (Crypt (pubK A) \<lbrace>Number Na, Number Nb, Agent B\<rbrace>) # evs2 \<in> nspublic"
 
-| NS3: "\<lbrakk>evs3 \<in> nspublic;
-         Says A B (Crypt (shrK A) \<lbrace>Nonce Na, Agent A\<rbrace>) \<in> set evs3;
-         Says B A (Crypt (shrK B) \<lbrace>Number Na, Number Nb, Agent B\<rbrace>) \<in> set evs3\<rbrakk> 
-\<Longrightarrow> Says A B (Crypt (shrK A) (Nonce Nb)) # evs3 \<in> nspublic"
+  | NS3: "\<lbrakk>evs3 \<in> nspublic;
+           Says A B (Crypt (pubK B) \<lbrace>Nonce Na, Agent A\<rbrace>) \<in> set evs3;
+           Says B' A (Crypt (pubK A) \<lbrace>Number Na, Number Nb, Agent B\<rbrace>) \<in> set evs3\<rbrakk> 
+    \<Longrightarrow> Says A B (Crypt (pubK B) (Nonce Nb)) # evs3 \<in> nspublic"
+
+lemma Spy_see_keys_for_compromised_agents :
+  "evs \<in> nspublic \<Longrightarrow> (Key (priK A) \<in> parts (knows Spy evs)) = (A \<in> bad)"
+  apply (erule nspublic.induct)
+  apply (simp_all)
+  apply auto
+  done
 
 end
