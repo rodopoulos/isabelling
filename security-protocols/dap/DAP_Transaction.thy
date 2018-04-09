846cd1de64cfb4d86cb6fb637ed6a0fb855e7413
Felipe Rodopoulos <felipekss@gmail.com>
2018-04-09 18:25:20 -0300

    Fixed transaction definition

--- security-protocols/dap/DAP_Transaction.thy
+++ security-protocols/dap/DAP_Transaction.thy
@@ -11,12 +11,12 @@ inductive_set daptrans :: "event list set" where
   Nil: "[] \<in> daptrans"
 
   | DT1: "\<lbrakk> evs1 \<in> daptrans \<rbrakk>
-    \<Longrightarrow> Says A Server Transaction # evs1 \<in> daptrans"
+    \<Longrightarrow> Says A Server (Number T) # evs1 \<in> daptrans"
 
   | DT2: "\<lbrakk> evs2 \<in> daptrans;
           Nonce r \<notin> used evs2;
           Says A Server Transaction \<in> set evs2;
-          r' = Crypt (shrK K2) (Nonce r);
+          r' = Crypt K2 (Nonce r);
           h_s = Hash \<lbrace> Transaction, r' \<rbrace>
         \<rbrakk>
     \<Longrightarrow> Says Server A \<lbrace> Transaction, r', h_s \<rbrace> # evs2 \<in> daptrans"
@@ -24,11 +24,22 @@ inductive_set daptrans :: "event list set" where
   | DT3: "\<lbrakk> evs3 \<in> daptrans;
           Says A Server Transaction \<in> set evs3;
           Says S A \<lbrace> Transaction', r', h_s \<rbrace> \<in> set evs3;
-          h_u = Hash \<lbrace> Transaction, r' \<rbrace>;
+          h_u = Hash \<lbrace> Transaction', r' \<rbrace>;
           h_s = h_u 
         \<rbrakk> 
     \<Longrightarrow> Says A Server (Nonce r) # evs3 \<in> daptrans"
 
   | Fake: "\<lbrakk>evsf \<in> daptrans; X \<in> synth(analz(spies evsf))\<rbrakk> \<Longrightarrow> Says Spy B X # evsf \<in> daptrans"
+
+
+lemma Protocol_terminates :
+  "\<exists>r. \<exists>evs \<in> daptrans. Says A Server (Nonce r) \<in> set evs"
+  apply (intro exI bexI)
+  apply (rule_tac [2] daptrans.DT3)
+  apply (rule_tac [2] daptrans.DT2)
+  apply (rule_tac [2] daptrans.DT1)
+  apply (rule_tac [2] daptrans.Nil)
+  apply(possibility)
+  done
 
 end
