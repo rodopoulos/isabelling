0d85b11db726baa5b08f893c3cc3a60fa0a37577
Felipe Rodopoulos <felipekss@gmail.com>
2018-04-09 18:03:54 -0300

    First inductive model definition for DAP transactions

---
+++ security-protocols/dap/DAP_Transaction.thy
@@ -0,0 +1,34 @@
+(*
+  Title: Dynamic Authorization Protocol - Message Transaction
+  Author: Felipe Rodopoulos de Oliveira
+*)
+
+theory DAP_Transaction imports "~~/src/HOL/Auth/Public"
+
+begin
+
+inductive_set daptrans :: "event list set" where
+  Nil: "[] \<in> daptrans"
+
+  | DT1: "\<lbrakk> evs1 \<in> daptrans \<rbrakk>
+    \<Longrightarrow> Says A Server Transaction # evs1 \<in> daptrans"
+
+  | DT2: "\<lbrakk> evs2 \<in> daptrans;
+          Nonce r \<notin> used evs2;
+          Says A Server Transaction \<in> set evs2;
+          r' = Crypt (shrK K2) (Nonce r);
+          h_s = Hash \<lbrace> Transaction, r' \<rbrace>
+        \<rbrakk>
+    \<Longrightarrow> Says Server A \<lbrace> Transaction, r', h_s \<rbrace> # evs2 \<in> daptrans"
+
+  | DT3: "\<lbrakk> evs3 \<in> daptrans;
+          Says A Server Transaction \<in> set evs3;
+          Says S A \<lbrace> Transaction', r', h_s \<rbrace> \<in> set evs3;
+          h_u = Hash \<lbrace> Transaction, r' \<rbrace>;
+          h_s = h_u 
+        \<rbrakk> 
+    \<Longrightarrow> Says A Server (Nonce r) # evs3 \<in> daptrans"
+
+  | Fake: "\<lbrakk>evsf \<in> daptrans; X \<in> synth(analz(spies evsf))\<rbrakk> \<Longrightarrow> Says Spy B X # evsf \<in> daptrans"
+
+end
